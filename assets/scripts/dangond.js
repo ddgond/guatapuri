3e766157e44aaf47aa046863604858844203ef33
Daniel Dangond <dadangond@gmail.com>
2019-04-16 19:43:49 -0400

    Connected everything

--- assets/scripts/dangond.js
+++ assets/scripts/dangond.js
@@ -3,14 +3,14 @@ function goTo(target) {
     console.log("story");
     document.getElementById("fadeToBlack").className = "activeFade";
     setInterval(function() {
-      window.location = "file:///Users/dangond/Desktop/story.html";
+      window.location = "/story.html";
     }, 3500);
   }
   if (target == "sound") {
     console.log("sound");
     document.getElementById("fadeToBlack").className = "activeFade";
     setInterval(function() {
-      window.location = "file:///Users/dangond/Desktop/sound.html";
+      window.location = "/music";
     }, 3500);
   }
 }
